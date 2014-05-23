1767601eeeee310eb131bd04e03bc154e51b143f
Jan Stette <jans@blinkbox.com>
2014-05-23 17:42:24 +0100

    Added directive for setting standard cache headers on cached resources

--- src/main/scala/com/blinkbox/books/spray/Directives.scala
+++ src/main/scala/com/blinkbox/books/spray/Directives.scala
@@ -1,8 +1,13 @@
 package com.blinkbox.books.spray
 
-import spray.routing.HttpService
-
 import JsonFormats._
+import scala.concurrent.duration.Duration
+import shapeless._
+import spray.http.DateTime
+import spray.http.HttpHeaders._
+import spray.http.CacheDirectives._
+import spray.routing.Directive1
+import spray.routing.HttpService
 
 case class Page(offset: Int, count: Int) {
   require(offset >= 0, "Offset must be 0 or greater")
@@ -16,4 +21,22 @@ trait Directives {
    * Custom directive for extracting and validating page parameters (offset and count).
    */
   def paged(defaultCount: Int) = parameters('offset.as[Int] ? 0, 'count.as[Int] ? defaultCount).as(Page)
+
+  /**
+   * Directive for setting cache headers in a standard way.
+   *
+   * This sets time-dependent headers without requiring the use of a surrounding dynamic {} directive.
+   *
+   * @param maxAge  The duration for which the returned value is cacheable.
+   */
+  def cacheable(maxAge: Duration) = provideTimeNow.flatMap { now => withCacheHeaders(now, maxAge) }
+
+  /** Get the current time, evaluated on each invocation of the directive. */
+  private def provideTimeNow: Directive1[DateTime] = extract(ctx => DateTime.now)
+
+  /** Set cache headers. */
+  private def withCacheHeaders(now: DateTime, maxAge: Duration) = respondWithHeaders(
+    `Cache-Control`(`public`, `max-age`(maxAge.toSeconds)),
+    RawHeader("Expires", (now + maxAge.toMillis).toRfc1123DateTimeString))
+
 }
---
+++ src/test/scala/com/blinkbox/books/spray/CachableDirectiveTest.scala
@@ -0,0 +1,78 @@
+package com.blinkbox.books.spray
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import scala.concurrent.duration._
+import spray.http.DateTime
+import spray.http.StatusCodes._
+import spray.routing.{ HttpService, Route }
+import spray.testkit.ScalatestRouteTest
+import scala.util.Try
+
+/**
+ * A trait that contains a route using the 'paged' directive.
+ */
+private[spray] trait CacheableService extends HttpService with Directives {
+
+  val maxAge = 1.minute
+
+  // Test route.
+  // This is defined as a val to check that headers are set dynamically even with a static route.
+  val route: Route = get {
+    path("cacheableEndpoint") {
+      cacheable(maxAge) {
+        complete(OK)
+      }
+    } ~
+      path("nonCacheableEndpoint") {
+        complete(OK)
+      }
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class CacheableDirectiveTest extends FunSuite with ScalatestRouteTest with CacheableService {
+
+  override implicit def actorRefFactory = system
+
+  test("Response headers on cacheable endpoint") {
+    val start = DateTime.now
+    Get("/cacheableEndpoint") ~> route ~> check {
+      assert(status === OK)
+      assert(header("Cache-Control").get.value === s"public, max-age=${maxAge.toSeconds}")
+
+      val expiryTimeStr = header("Expires").get.value
+      assert(parseDateTime(expiryTimeStr).isSuccess, "Should have set expiry time in the valid format")
+    }
+  }
+
+  test("Response headers are updated dynamically") {
+    Get("/cacheableEndpoint") ~> route ~> check {
+      val time1 = parseDateTime(header("Expires").get.value).get
+
+      // Sleep for a second, to ensure date header will be updated.
+      Thread.sleep(2001)
+      Get("/cacheableEndpoint") ~> route ~> check {
+        assert(status === OK)
+        val time2 = parseDateTime(header("Expires").get.value).get
+        assert(time1 != time2, "Time field should be updated dynamically on each request")
+      }
+    }
+  }
+
+  test("No cache related response headers on non-cacheable endpoint") {
+    Get("/nonCacheableEndpoint") ~> route ~> check {
+      assert(status === OK)
+      assert(header("Cache-Control") === None)
+      assert(header("Expires") === None)
+      assert(header("Date") === None)
+    }
+  }
+
+  // The expected date-time format.
+  private val dateTimeFormatter = org.joda.time.format.DateTimeFormat.forPattern("E, d MMM yyyy HH:mm:ss z")
+
+  def parseDateTime(str: String) = Try(DateTime(dateTimeFormatter.parseDateTime(str).getMillis()))
+
+}
