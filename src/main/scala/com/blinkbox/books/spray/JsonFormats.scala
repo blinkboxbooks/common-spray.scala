628dcff7714019e46ec4803fe9ea45f82d6a1189
Greg Beech <greg@blinkbox.com>
2014-08-06 14:45:29 +0100

    Replaced some code with common-json
    ### Breaking changes
    
    - `JsonFormats`.`ExplicitTypeHints` and
    `JsonFormats.ISODateTimeSerializer` have been removed; use the
    corresponding classes in `common-json`.
    
    ### Bug fixes
    
    - DateTime query parameters are now parsed correctly if they include a
    milliseconds component.
    - DateTime query parameters are always converted to UTC even if they
    were sent in a different time zone.

--- src/main/scala/com/blinkbox/books/spray/JsonFormats.scala
+++ src/main/scala/com/blinkbox/books/spray/JsonFormats.scala
@@ -1,8 +1,8 @@
 package com.blinkbox.books.spray
 
-import org.joda.time.DateTime
-import org.joda.time.format.ISODateTimeFormat
-import org.json4s.JsonAST.{JNull, JString}
+import com.blinkbox.books.json.DefaultFormats
+import org.joda.time.format.{DateTimeFormatterBuilder, ISODateTimeFormat}
+import org.joda.time.{DateTime, DateTimeZone}
 import org.json4s._
 import spray.httpx.unmarshalling.{FromStringDeserializer, MalformedContent}
 
@@ -14,35 +14,13 @@ import scala.util.control.NonFatal
 object JsonFormats {
 
   /**
-   * Class that allows custom strings to be used as type hints for classes, by passing in a map
-   * of classes to the strings that they should be tagged with in generated JSON.
-   */
-  case class ExplicitTypeHints(customHints: Map[Class[_], String]) extends TypeHints {
-    private val hintToClass = customHints.map(_.swap)
-    override val hints = customHints.keys.toList
-    override def hintFor(clazz: Class[_]) = customHints.get(clazz).get
-    override def classFor(hint: String) = hintToClass.get(hint)
-  }
-
-  /**
-   * Serializer for Joda DateTime objects in the ISO date format. This ensures that time zone
-   * information is not lost.
-   */
-  case object ISODateTimeSerializer extends CustomSerializer[DateTime](_ => ({
-    case JString(s) => ISODateTimeFormat.dateTime().parseDateTime(s)
-    case JNull => null
-  }, {
-    case d: DateTime => JString(ISODateTimeFormat.dateTime().print(d))
-  }))
-
-  /**
    * Deserializer to parse BigDecimal values in query parameters
    */
   implicit val BigDecimalDeserializer = new FromStringDeserializer[BigDecimal] {
     def apply(value: String) =
       try Right(BigDecimal(value))
       catch {
-        case NonFatal(ex) => Left(MalformedContent("'%s' is not a valid 128-bit BigDecimal value" format value, ex))
+        case NonFatal(ex) => Left(MalformedContent(s"'$value' is not a valid 128-bit BigDecimal value", ex))
       }
   }
 
@@ -50,10 +28,12 @@ object JsonFormats {
    * Deserializer to parse Joda DateTime values in query parameters
    */
   implicit val ISODateTimeDeserializer = new FromStringDeserializer[DateTime] {
+    val parsers = Array(ISODateTimeFormat.dateTime.getParser, ISODateTimeFormat.dateTimeNoMillis.getParser)
+    val format = new DateTimeFormatterBuilder().append(null, parsers).toFormatter
     def apply(value: String) =
-      try Right(ISODateTimeFormat.dateTimeNoMillis().parseDateTime(value))
+      try Right(format.parseDateTime(value).withZone(DateTimeZone.UTC))
       catch {
-        case NonFatal(ex) => Left(MalformedContent("'%s' is not a valid yyyy-MM-dd'T'HH:mm:ss'Z date format value" format value, ex))
+        case NonFatal(ex) => Left(MalformedContent(s"'$value' is not a valid ISO date format value", ex))
       }
   }
 
@@ -68,7 +48,6 @@ object JsonFormats {
   def blinkboxFormat(hints: TypeHints = NoTypeHints): Formats = new DefaultFormats {
     override val typeHints: TypeHints = hints
     override val typeHintFieldName: String = "type"
-    override val wantsBigDecimal: Boolean = true // for endpoints that deal with money
-  } + ISODateTimeSerializer
+  }
 
 }
