c9015a71c5dc855667d850bc5a9388d54f247d9f
Egidijus Jankauskas <egidijus@blinkbox.com>
2014-11-19 11:22:50 +0000

    Refactored Throwable -> Error transformation

--- src/main/scala/com/blinkbox/books/spray/v2/Implicits.scala
+++ src/main/scala/com/blinkbox/books/spray/v2/Implicits.scala
@@ -2,11 +2,7 @@ package com.blinkbox.books.spray.v2
 
 import com.blinkbox.books.json.DefaultFormats
 import org.json4s.jackson.Serialization
-import spray.http.StatusCodes._
-import spray.http.{RequestProcessingException, IllegalRequestException}
 import spray.httpx.marshalling.Marshaller
-
-import scala.util.control.NonFatal
 
 object Implicits {
   /**
@@ -14,11 +10,6 @@ object Implicits {
    */
   implicit def throwableMarshaller: Marshaller[Throwable] =
     Marshaller.delegate[Throwable, String](`application/vnd.blinkbox.books.v2+json`) { t =>
-      val error = t match {
-        case e: IllegalRequestException => Error(e.status.reason.replace(" ", ""), Some(e.status.defaultMessage))
-        case e: RequestProcessingException => Error(e.status.reason.replace(" ", ""), Some(e.status.defaultMessage))
-        case NonFatal(e) => Error("InternalServerError", Some(InternalServerError.defaultMessage))
-      }
-      Serialization.write(error)(DefaultFormats)
+      Serialization.write(Error(t))(DefaultFormats)
     }
 }
