966a8fe5d148a4c71b77ae6455254532da770ddb
Greg Beech <greg@blinkbox.com>
2014-11-27 11:39:47 +0000

    Fixed a bug and updated metrics library
    ### Bug fixes
    
    - The error body for `RejectedExecutionException` being thrown is now
    correctly reported as service unavailable rather than internal server
    error.
    
    ### Improvements
    
    - Updated the version of the metrics library, and a couple of other
    shared libraries.

--- src/main/scala/com/blinkbox/books/spray/v2/Models.scala
+++ src/main/scala/com/blinkbox/books/spray/v2/Models.scala
@@ -1,9 +1,10 @@
 package com.blinkbox.books.spray.v2
 
 import java.net.URI
+import java.util.concurrent.RejectedExecutionException
 
 import spray.http.StatusCodes._
-import spray.http.{RequestProcessingException, IllegalRequestException}
+import spray.http.{StatusCode, RequestProcessingException, IllegalRequestException}
 
 import scala.util.control.NonFatal
 
@@ -19,9 +20,11 @@ case class Link(rel: Relation, url: URI)
 case class Error(code: String, developerMessage: Option[String])
 
 object Error {
+  def apply(status: StatusCode): Error = Error(status.reason.replace(" ", ""), Some(status.defaultMessage))
   def apply(t: Throwable): Error = t match {
-    case e: IllegalRequestException => Error(e.status.reason.replace(" ", ""), Some(e.status.defaultMessage))
-    case e: RequestProcessingException => Error(e.status.reason.replace(" ", ""), Some(e.status.defaultMessage))
-    case NonFatal(e) => Error("InternalServerError", Some(InternalServerError.defaultMessage))
+    case e: IllegalRequestException => Error(e.status)
+    case e: RequestProcessingException => Error(e.status)
+    case e: RejectedExecutionException => Error(ServiceUnavailable)
+    case NonFatal(e) => Error(InternalServerError)
   }
- }
+}
